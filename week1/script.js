8d39aab0637087ce65957999854060da641b27e6
Sam O'Neal <sam.oneal@gmail.com>
2023-04-10 00:56:05 -0700

    Week 1  introduction exercise.

---
+++ week1/script.js
@@ -0,0 +1,15 @@
+const firstName = "Sam";
+const lastName = "O'Neal";
+const yearsOfStudy = "three quarters";
+const goal = "become a web developer";
+
+function introduce(first, last, years, goal) {
+    return `My name is ${first} ${last}. I have been at SCC for ${years} and plan to ${goal} after completing my courses.`;
+}
+
+function main() {
+    message = introduce(firstName, lastName, yearsOfStudy, goal);
+    console.log(message);
+}
+
+main();
