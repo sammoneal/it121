e31037dc3039a901d7bd8df9d5a671af8fdcdc0a
Sam O'Neal <sam.oneal@gmail.com>
2023-06-15 10:21:23 -0700

    Week 9 assignment.

---
+++ week9/extract.js
@@ -0,0 +1,38 @@
+//select input area
+const firstArea = document.getElementById('txtarea');
+
+//select output area
+const secArea = document.getElementById('txtarea2');
+
+//Add event listener to button
+document.getElementsByTagName('button')[0].addEventListener('click', lookUp);
+
+//array for valid emails
+//needs to persist between clicks
+let holder = [];
+
+//email validator
+function validateEmail(email) {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
+
+//clears the output area then populates with holder values
+function pushHolder(){
+    secArea.value = ''
+    secArea.value = holder.join(';')
+}
+
+//parses input from first area
+function lookUp() {
+    const rawTxt = firstArea.value;
+    //delimit with semicolon
+    const eData = rawTxt.split(';');
+    for (let i = 0; i < eData.length; i++)
+        //if email is new and valid then add to holder
+        if (!holder.includes(eData[i]) && validateEmail(eData[i])){
+            holder.push(eData[i]);
+        }
+    //update output area
+    pushHolder();
+}
